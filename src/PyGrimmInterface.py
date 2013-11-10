137ebfd633c01e534f774c8c6b7900d0b6a4a5af
dcdanko <goblinbenches@bennigan>
2013-11-10 15:34:38 -0500

    genome class and grimm interface class

---
+++ src/PyGrimmInterface.py
@@ -0,0 +1,10 @@
+
+
+class PyGrimmInterface( object ):
+
+
+	def getTransformations( self, gA, gB):
+		pass
+
+	def getDistMatrix( self, genomes):
+		pass
---
+++ src/genome.py
@@ -0,0 +1,37 @@
+import RuntimeException
+
+
+class Genome( object ):
+
+	def __init__( self, grimmString=None):
+		self.grimmString = grimmString
+
+	def getChromosoneArray( self ):
+		g = []
+		for line in self.grimmString.split("\n"):
+			g += line.split("$")
+
+		if ">" in g[0]:
+			g = g[1:]
+
+		return g
+
+	def __iter__( self ):
+		return iter( self.getChromosoneArray())
+
+	def __str__( self ):
+		return self.grimmString
+
+	def addChromosone( self, chromosone ):
+		if "$" not in chromosone:
+			chromosone += " $"
+
+		self.grimmString += "\n" + chromosone
+
+	def addHeader( self, header ):
+		if ">" not in self.getChromosoneArray()[0]:
+			if ">" not in header:
+				header = "> " + header
+			self.grimmString = header + self.grimmString
+		else:
+			raise RuntimeException("Genome already has header") 
